1a7a453424478d89d4a8c4f3d27732559ec8e650
Albert TAZEGUL <atazegul1@myges.fr>
2021-05-19 18:45:58 +0200

    Nouvelle fonctionnalité

---
+++ fichier.c
@@ -0,0 +1,12 @@
+  
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	int testInteger;
+    	printf("Entrez votre prenom : ");
+    	scanf("%d", &testInteger);  
+    	printf("Salut = %d",testInteger);
+    	return 0;
+}
